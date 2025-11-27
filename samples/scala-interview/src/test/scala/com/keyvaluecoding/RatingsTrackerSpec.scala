d3ece75d98e1133dcc66b1ae42bff3ac344eaf43
Lachlan Deck <lachlan.deck@gmail.com>
2025-11-28 09:00:05 +1100

    epic-1[scala]: refactor tests to ease contract testing

--- samples/scala-interview/src/test/scala/com/keyvaluecoding/RatingsTrackerSpec.scala
+++ samples/scala-interview/src/test/scala/com/keyvaluecoding/RatingsTrackerSpec.scala
@@ -5,17 +5,23 @@ import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
 
 class RatingsTrackerSpec extends AnyFlatSpec with should.Matchers:
-  "The tracker" should "be empty by default" in:
-    val tracker = RatingsTracker()
-    tracker.top shouldBe -1
 
-  it should "handle a single product rating" in:
+  trait Tester:
     val tracker = RatingsTracker()
-    tracker.addRating(productId = 1, rating = 2)
-    tracker.top shouldBe 1
 
-  it should "handle multiple product ratings" in:
-    val tracker = RatingsTracker()
-    tracker.addRating(productId = 1, rating = 2)
-    tracker.addRating(productId = 2, rating = 1)
-    tracker.top shouldBe 1
+    def ingest(s: Rating*): Unit =
+      s.foreach(r => tracker.addRating(productId = r.productId, rating = r.rating))
+
+    def top = tracker.top
+
+
+  "The tracker" should "be empty by default" in new Tester:
+    top shouldBe -1
+
+  it should "handle a single product rating" in new Tester:
+    ingest(Rating(productId = 1, rating = 2))
+    top shouldBe 1
+
+  it should "handle multiple product ratings" in new Tester:
+    ingest(Rating(productId = 1, rating = 2),Rating(productId = 2, rating = 1))
+    top shouldBe 1
